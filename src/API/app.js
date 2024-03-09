b3e4cf915cc72be4836421e365f2c296bfb50b4b
Brent Nesbitt <brent.nesbitt@p2c.com>
2024-03-09 09:05:24 -0800

    comment unused code

--- src/API/app.js
+++ src/API/app.js
@@ -2,8 +2,8 @@ const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
 
-const { OAuth2Client } = require('google-auth-library');
-const oauth2Client = new OAuth2Client()
+// const { OAuth2Client } = require('google-auth-library');
+// const oauth2Client = new OAuth2Client()
 
 const app = express();
 
