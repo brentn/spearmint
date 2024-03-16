d1ea87ae5d79045b89b3dc991dfd0ab48516e5a1
Brent Nesbitt <brent.nesbitt@p2c.com>
2024-03-15 18:39:00 -0700

    Add Account button working in sandbox

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from '../app.module';
+import { linkToken } from '../data/state/selectors';
+import { PlaidOnSuccessArgs } from 'ngx-plaid-link';
 
 @Component({
   selector: 'app-new-account',
@@ -6,5 +10,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./new-account.component.css']
 })
 export class NewAccountComponent {
+  @ViewChild('plaid') plaid: ElementRef | undefined;
+  linkToken$ = this.store.select(linkToken);
+
+  constructor(private store: Store<AppState>) { }
+
+  ngAfterViewInit(): void {
+    const startTime = Date.now();
+    while (!this.plaid && (Date.now() - startTime < 1000)) { }
+    if (this.plaid) {
+      const button = this.plaid.nativeElement;
+      setTimeout(() => button.click(), 1000);
+    } else {
+      console.log('BUTTON NOT FOUND')
+    }
+  }
+
+  onPlaidSuccess(evt: PlaidOnSuccessArgs): void {
+    console.log('HERE', evt)
+  }
 
 }
