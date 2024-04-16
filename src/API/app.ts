eb67e7300262982c3541bfccce7fbe001055bf68
Brent Nesbitt <brent.nesbitt@p2c.com>
2024-04-16 07:01:22 -0700

    change logging

--- src/API/app.ts
+++ src/API/app.ts
@@ -86,7 +86,7 @@ app.post('/register', async (req: Request, res: Response) => {
 app.post('/authenticate', async (req: Request, res: Response) => {
   try {
     const credentialId = req.body.credentialId;
-    console.log('CREDENTIAL', req.headers['userId'], credentialId, JSON.parse(localStorage.getItem('credentials') || '[]').map((a: any) => a.credential.id));
+    console.log('CREDENTIAL', req.body, JSON.parse(localStorage.getItem('credentials') || '[]').map((a: any) => a.credential.id));
     const credentialKey = JSON.parse(localStorage.getItem('credentials') || '[]').find((a: { credential: { id: string } }) => a.credential.id === credentialId)?.credential;
     if (!credentialKey) { throw new Error('Credential not found'); }
     const challenge: string = localStorage.getItem('challenge');
