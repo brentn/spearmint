e61f26384b498ce5bc94f91c7bfe1005a1be3899
Brent Nesbitt <brent.nesbitt@p2c.com>
2024-04-17 21:17:08 -0700

    Add logging to authenticate

--- src/API/app.ts
+++ src/API/app.ts
@@ -80,13 +80,14 @@ app.post('/authenticate', async (req: Request, res: Response) => {
   try {
     const credentialId = req.body.credentialId;
     const credential = await db.getCredential(credentialId);
+    console.log('Credential:', credentialId, credential.id)
     if (!credential) { throw new Error('Credential not found'); }
     const challenge: string = await db.getChallenge();
     await server.verifyAuthentication(req.body, credential, {
       challenge,
       origin: (origin: string) => allowedOrigins?.includes(origin),
       userVerified: true,
-      verbose: false
+      verbose: true
     });
     await db.clearChallenge(challenge);
     res.status(200).json('');
